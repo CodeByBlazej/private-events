391dd5ab052865df7c3592f0913dca5cc126cdf9
CodeByBlazej <codebyblazej@gmail.com>
2026-01-25 13:17:16 +0000

    add reservation table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_24_151450) do
+ActiveRecord::Schema[8.0].define(version: 2026_01_25_131458) do
   create_table "events", force: :cascade do |t|
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "creator_id", null: false
     t.index ["creator_id"], name: "index_events_on_creator_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.integer "event_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_reservations_on_event_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +41,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_24_151450) do
   end
 
   add_foreign_key "events", "users", column: "creator_id"
+  add_foreign_key "reservations", "events"
+  add_foreign_key "reservations", "users"
 end
